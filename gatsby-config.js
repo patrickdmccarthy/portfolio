ff9718ccf96b19900fa9c57fd1d186e3b529018e
Patrick McCarthy <patrick.d.mccarthy@gmail.com>
2019-05-04 20:23:38 +0200

    add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,15 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-139561909-1',
+        head: false,
+        anonymize: true,
+        respectDNT: true,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
