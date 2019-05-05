2fff065c45065044a5b54db51fcf232165473924
Patrick McCarthy <patrick.d.mccarthy@gmail.com>
2019-05-05 15:27:44 +0200

    add sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-lodash`,
     {
       resolve: `gatsby-source-filesystem`,
