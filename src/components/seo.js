123eb921c4f82ca06bb415d1917e0b5089fb6bc4
Patrick McCarthy <patrick.d.mccarthy@gmail.com>
2019-05-05 14:38:00 +0200

    add host to meta image

--- src/components/seo.js
+++ src/components/seo.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 import monogram from "../images/monogram.svg"
 
-const SEO = ({ title, description, image, pathname, article }) => (
+const SEO = ({ title, description, pathname, article }) => (
   <StaticQuery
     query={query}
     render={({
@@ -14,7 +14,6 @@ const SEO = ({ title, description, image, pathname, article }) => (
           titleTemplate,
           defaultDescription,
           siteUrl,
-          defaultImage,
           twitterUsername,
         },
       },
@@ -22,7 +21,7 @@ const SEO = ({ title, description, image, pathname, article }) => (
       const seo = {
         title: title || defaultTitle,
         description: description || defaultDescription,
-        image: `${siteUrl}${image || defaultImage}`,
+        image: `${siteUrl}${monogram}`,
         url: `${siteUrl}${pathname || "/"}`,
       }
 
@@ -30,7 +29,7 @@ const SEO = ({ title, description, image, pathname, article }) => (
         <>
           <Helmet title={seo.title} titleTemplate={titleTemplate}>
             <meta name="description" content={seo.description} />
-            <meta name="image" content={monogram} />
+            <meta name="image" content={seo.image} />
             {seo.url && <meta property="og:url" content={seo.url} />}
             {(article ? true : null) && (
               <meta property="og:type" content="article" />
@@ -39,7 +38,7 @@ const SEO = ({ title, description, image, pathname, article }) => (
             {seo.description && (
               <meta property="og:description" content={seo.description} />
             )}
-            {monogram && <meta property="og:image" content={monogram} />}
+            {seo.image && <meta property="og:image" content={seo.image} />}
             <meta name="twitter:card" content="summary_large_image" />
             {twitterUsername && (
               <meta name="twitter:creator" content={twitterUsername} />
@@ -48,7 +47,7 @@ const SEO = ({ title, description, image, pathname, article }) => (
             {seo.description && (
               <meta name="twitter:description" content={seo.description} />
             )}
-            {monogram && <meta name="twitter:image" content={monogram} />}
+            {seo.image && <meta name="twitter:image" content={seo.image} />}
           </Helmet>
         </>
       )
@@ -82,7 +81,6 @@ const query = graphql`
         titleTemplate
         defaultDescription: description
         siteUrl: url
-        defaultImage: image
         twitterUsername
       }
     }
